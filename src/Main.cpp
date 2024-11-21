e885582e7a569caa11e04c70a533762f74ee768d
Hesselst <hesselsteenwoerd@gmail.com>
2024-11-21 15:36:48 +0100

    Everything works except the ball and the score
    Will add physics for the ball later and also the score system

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include <conio.h>
+#include "Windows.h"
+
+#include "Main.h"
 
 bool gameOver;
 const int width = 35;
 const int height = 15;
 int score;
-const int leftX = 2;
-const int rightX = 30;
+const int leftX = 0;
+const int rightX = 34;
 int leftY, rightY;
 int ballX, ballY;
-const int speed = 2;
 const enum playerDirection { stop=0, up, down };
 playerDirection leftDir;
 playerDirection rightDir;
@@ -19,9 +21,10 @@ int main()
     setup();
     while (!gameOver)
     {
-        draw();
         input();
         logic();
+        draw();
+        Sleep(15);
     }
 }
 
@@ -41,33 +44,88 @@ void logic()
 {
     if (rightDir == down)
     {
-
+        if (rightY != 13)
+        {
+            rightY += 1;
+        }
     }
     if (rightDir == up)
     {
-
+        if (rightY != 2)
+        {
+            rightY -= 1;
+        }
     }
     if (leftDir == down)
     {
-
+        if (leftY != 13)
+        {
+            leftY += 1;
+        }
     }
     if (leftDir == up)
     {
-
+        if (leftY != 2)
+        {
+            leftY -= 1;
+        }
     }
 }
 
 void draw()
 {
+    system("cls");
+    // prob have to add 2 score variables for left and right
+    std::cout << "Score: " << score << " - " << score << std::endl;
 
+    // i for y axis
+    for (int i = 0; i < height + 1; i++)
+    {
+        // j for x axis
+        for (int j = 0; j < width; j++)
+        {
+            // Draw the bounds
+            if (i == 0)
+            {
+                std::cout << "#";
+            }
+            else if (i == 15)
+            {
+                std::cout << "#";
+            }
+            // Draw the left player
+            else if ((j == leftX && i == leftY) || (j == leftX && i == leftY + 1) || (j == leftX && i == leftY - 1))
+            {
+                std::cout << "e";
+            }
+            // Draw tje right player
+            else if ((j == rightX && i == rightY) || (j == rightX && i == rightY + 1) || (j == rightX && i == rightY - 1))
+            {
+                std::cout << "a";
+            }
+            // Draw the ball
+            else if (ballX == j && ballY == i)
+            {
+                std::cout << "0";
+            }
+            else
+            {
+                std::cout << " ";
+            }
+        }
+        std::cout << "\n";
+    }
+
+    std::cout << "Press q to exit";
 }
 
 void input()
 {
-    if (kbhit())
+    if (_kbhit())
     {
-        switch (getch())
+        switch (_getch())
         {
+            // s and w keys for left player
         case 's':
         {
             leftDir = down;
@@ -78,7 +136,7 @@ void input()
             leftDir = up;
             break;
         }
-        // Up and down arrow keys but not sure if these are the correct codes
+        // Up and down arrow keys for right plauyer
         case 72:
         {
             rightDir = up;
@@ -89,25 +147,12 @@ void input()
             rightDir = down;
             break;
         }
+
+        case 'q':
+        {
+            gameOver = true;
+            break;
+        }
         }
     }
 }
-
-         Score: 0 - 1
-==============================
-
-        PRESS Y TO START
-
-                           a
-e                          a
-e                          a
-e
-                       0
-
-==============================
-
-1 2 3 4 5 6 7 8 9 10 11 12 13
-          6 7 8 
-
-Left side = w&s
-Right side = up&down arrow key
