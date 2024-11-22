6b345846d3c144e83e3768c52c67929a62736ea8
Hesselst <hesselsteenwoerd@gmail.com>
2024-11-22 10:36:05 +0100

    Added score and simple gameplay that only works horizontally

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <conio.h>
 #include "Windows.h"
+#include <string>
 
 #include "Main.h"
 
 bool gameOver;
 const int width = 35;
 const int height = 15;
-int score;
+int leftScore = 0;
+int rightScore = 0;
 const int leftX = 0;
 const int rightX = 34;
 int leftY, rightY;
@@ -15,6 +17,9 @@ int ballX, ballY;
 const enum playerDirection { stop=0, up, down };
 playerDirection leftDir;
 playerDirection rightDir;
+
+char prevHitPlayer = NULL;
+std::string playerScored = " ";
 
 int main()
 {
@@ -24,7 +29,8 @@ int main()
         input();
         logic();
         draw();
-        Sleep(15);
+        // The amount just really depends on ur fps so u might have to change this. maybe i will add deltatime but im not sure how to do that since im working with whole numbers in the console
+        Sleep(20);
     }
 }
 
@@ -37,11 +43,59 @@ void setup()
     rightDir = stop;
     rightY = 8;
     leftY = 8;
-    score = 0;
 }
 
 void logic()
 {
+    // Check if the ball collided with the right player
+    if (ballX == rightX)
+    {
+        if (ballY == rightY || ballY == (rightY - 1) || ballY == (rightY + 1))
+        {
+            prevHitPlayer = 'b';
+        }
+        else
+        {
+            playerScored = "Left";
+            leftScore++;
+            setup();
+        }
+    }
+
+    // Check if the ball collided with the left player
+    if (ballX == leftX)
+    {
+        if (ballY == leftY || ballY == (leftY - 1) || ballY == (leftY + 1))
+        {
+            prevHitPlayer = 'a';
+        }
+        else
+        {
+            playerScored = "Right";
+            rightScore++;
+            setup();
+        }
+    }
+
+    // If the ball hits the bounds, reverse the Y axis
+    if (ballY == 14)
+    {
+        ballY--;
+    }
+    if (ballY == 1)
+    {
+        ballY++;
+    }
+
+    if (prevHitPlayer == 'a' || prevHitPlayer == NULL)
+    {
+        ballX++;
+    }
+    if (prevHitPlayer == 'b')
+    {
+        ballX--;
+    }
+
     if (rightDir == down)
     {
         if (rightY != 13)
@@ -76,7 +130,15 @@ void draw()
 {
     system("cls");
     // prob have to add 2 score variables for left and right
-    std::cout << "Score: " << score << " - " << score << std::endl;
+    std::cout << "Score: " << leftScore << " - " << rightScore << std::endl;
+    if (playerScored != " ")
+    {
+        std::cout << playerScored << " player scored!" << std::endl;
+    }
+    else
+    {
+        std::cout << "No one has scored yet" << std::endl;
+    }
 
     // i for y axis
     for (int i = 0; i < height + 1; i++)
